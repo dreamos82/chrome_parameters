368ad4e766cb86d1579fd91d654e3073f00846e6
Ivan Gualandri <dreamos82@yahoo.it>
2021-11-04 19:59:15 +0000

    Show hostname next to the extension title

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,7 @@
  *
  * popup.js
  *
- * @version 1.5
+ * @version 1.8
  *
  */
 var exporters = {csv: create_csv, json: create_json, xml: create_xml};
@@ -176,26 +176,31 @@ function parse_url(current_url){
 	var par_start = current_url.indexOf('?');
 	var hash_position = current_url.lastIndexOf('#');
 	if(par_start>0 && par_start!= current_url.length-1){
-	if (hash_position>par_start) {
-		parameter_url = current_url.substring(++par_start, hash_position);
-	} else {
-		parameter_url = current_url.substring(++par_start);
-	}
-	var result = parameter_url.split("&");
-	document.getElementById("container").innerHTML = "";
-
-	for(var i=0;i<result.length && result.length>0; i++){
-		showParameter(result[i]);
-	}
-	if(hash_position>par_start){
-		/*Improve this piece of code*/
-		var sub_result = current_url.substring(++hash_position);
-		var sub_array = sub_result.split("&");
-		add_hash();
-		var k = 0;
-		for(k=0;k<sub_array.length && sub_array.length>0; k++){
-			console.log(sub_array[k]);
-			showParameter(sub_array[k]);
+    	if (hash_position>par_start) {
+    		parameter_url = current_url.substring(++par_start, hash_position);
+    	} else {
+	    	parameter_url = current_url.substring(++par_start);
+    	}
+        var result = parameter_url.split("&");
+    	document.getElementById("container").innerHTML = "";
+        var title_div = document.getElementById("title_div");
+        if( title_div != undefined){
+            var url_object = new URL(current_url);
+            var text_node = document.createTextNode(" for " + url_object.hostname);
+            title_div.appendChild(text_node);
+        }
+	    for(var i=0;i<result.length && result.length>0; i++){
+		    showParameter(result[i]);
+    	}
+	    if(hash_position>par_start){
+		    /*Improve this piece of code*/
+    		var sub_result = current_url.substring(++hash_position);
+	    	var sub_array = sub_result.split("&");
+		    add_hash();
+    		var k = 0;
+	    	for(k=0;k<sub_array.length && sub_array.length>0; k++){
+		    	console.log(sub_array[k]);
+			    showParameter(sub_array[k]);
 			}
 		}
 	}
